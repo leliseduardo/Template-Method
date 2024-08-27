b9ebbe1905a2a837e758514c89e21d0a5598bf36
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-08-26 22:00:17 -0300

    init

---
+++ src/test/java/org/example/template_method/TemplateMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.template_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateMethodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
